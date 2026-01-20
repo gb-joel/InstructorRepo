9feb8eb92a73d39e2598acf7809864acfb9843e6
gb-joel <240119397@aston.ac.uk>
2026-01-20 12:06:31 +0000

    Update Lab01_Java.java
    added a new comment

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -1,5 +1,6 @@
 //Lab01 Java file
 // Sample Java file
+ // Edit made by 240119397.
 void testMethod1()
   {
   //First command x = y + z
@@ -21,3 +22,4 @@ void main()
     testMethod1();
     testMethod2();
 }
+
